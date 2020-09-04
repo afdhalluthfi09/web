c89fb269e501e04a8a334a2b306d6a22f5014d20
afdhalluthfi09 <afdhal.luthfi@students.amikom.ac.id>
2020-09-05 00:36:56 +0700

    tambahan file

---
+++ server/src/app.js
@@ -0,0 +1,19 @@
+const express =require('express')
+const cors =require('cors')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
+
+const app = express()
+app.use(morgan('combine'))
+app.use(bodyParser.json())
+app.use(cors())
+
+
+
+app.get('/status',(req,res)=>{
+    res.send({
+        "message" :"hello internet saya lahir"
+    })
+})
+app.listen(process.env.PORT || 8081)
